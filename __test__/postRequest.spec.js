bedd389cf5f7fa781718794063a2605c0f8377d1
heavsta <francoisleduc@ymail.com>
2020-07-12 18:31:25 +0200

    added last test, now only service workers to do

--- __test__/postRequest.spec.js
+++ __test__/postRequest.spec.js
@@ -1,2 +1,14 @@
 import { postRequest } from '../src/client/js/postRequest'
 
+//using fetch-mock to mock our fetch call
+const fetchMock = require('fetch-mock')
+
+describe('Test post route', () => {
+    test('Checks if postRequest  ', async () => {
+        fetchMock.post('https://jestjs.io/', { polarity: "neutral" });
+        const response = await postRequest('https://jestjs.io/');
+
+        expect.assertions(1);
+        expect(response.polarity).toEqual('neutral');
+    })
+})
